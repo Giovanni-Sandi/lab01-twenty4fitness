d3e9ea7f90c210ba62afcc62b2dfeac68bc86450
Giovanni-Sandi <luisgiosandiff@gmail.com>
2026-06-08 22:21:41 -0600

    feat(landing-page): improve UI and fix responsive layout issues

--- js/app.js
+++ js/app.js
@@ -1,22 +1,83 @@
 const themeToggle = document.getElementById("theme-toggle");
 
+const body = document.body;
+
+const logo = document.getElementById("logo");
+
+const footerLogo = document.getElementById("footer-logo");
+
+/* =========================
+   CARGAR TEMA GUARDADO
+========================= */
+
 const savedTheme = localStorage.getItem("theme");
 
 if (savedTheme === "light") {
-  document.body.classList.add("light-mode");
-  themeToggle.textContent = "☀️";
+  body.classList.add("light-mode");
+
+  themeToggle.textContent = "☼";
+
+  if (logo) {
+    logo.src = "./img/logo/logo-light.png";
+  }
+
+  if (footerLogo) {
+    footerLogo.src = "./img/logo/logo-light.png";
+  }
+} else {
+  themeToggle.textContent = "☾";
+
+  if (logo) {
+    logo.src = "./img/logo/logo-dark.png";
+  }
+
+  if (footerLogo) {
+    footerLogo.src = "./img/logo/logo-dark.png";
+  }
 }
 
-themeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("light-mode");
+/* =========================
+   CAMBIAR TEMA
+========================= */
 
-  const isLightMode = document.body.classList.contains("light-mode");
+themeToggle.addEventListener("click", () => {
+  body.classList.toggle("light-mode");
+
+  const isLightMode = body.classList.contains("light-mode");
 
   if (isLightMode) {
     localStorage.setItem("theme", "light");
-    themeToggle.textContent = "☀️";
+
+    themeToggle.textContent = "☼";
+
+    if (logo) {
+      logo.src = "./img/logo/logo-light.png";
+    }
+
+    if (footerLogo) {
+      footerLogo.src = "./img/logo/logo-light.png";
+    }
   } else {
     localStorage.setItem("theme", "dark");
-    themeToggle.textContent = "🌙";
+
+    themeToggle.textContent = "☾";
+
+    if (logo) {
+      logo.src = "./img/logo/logo-dark.png";
+    }
+
+    if (footerLogo) {
+      footerLogo.src = "./img/logo/logo-dark.png";
+    }
   }
+});
+
+/* MENU MOBILE */
+
+const menuToggle = document.getElementById("menu-toggle");
+
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
